17ced32c44404254c5b51e79a3df1e1c5810ceba
Mauro Mascarenhas de Araújo <mauro.mascarenhas@live.com>
2017-01-22 02:36:17 -0200

    Sources added | Adicionado código
    Source code added (V 1.0)
    
    PT
    
    Código fonte adicionado (V1.0)

---
+++ safeIt.js
@@ -0,0 +1,111 @@
+/*-----------------------------------------------------------------------------------
+	Author: Mauro Mascarenhas
+	Organization link: https://www.nintersoft.com/
+	Last update: 21/01/2017
+	Version: 1.0
+-----------------------------------------------------------------------------------*/
+
+//------------------------------------ LISTENER -------------------------------------
+
+browser.webRequest.onBeforeRequest.addListener(
+  chooseSafe,
+  {urls: ["<all_urls>"]},
+  ["blocking"]
+);
+
+//------------------------------------ FUNCTIONS -------------------------------------
+
+function chooseSafe(requestDetails){
+	
+	// ---- Variable declarations ----
+	var URL = requestDetails.url;
+	var pGoogle = new RegExp(".google.");
+	var pBing = new RegExp(".bing.");
+	var pYahoo = new RegExp("search.yahoo.com");
+	var pYandex = new RegExp("yandex.");
+	var pSearch = new RegExp("/search");
+	var urlTemp;
+	
+	var canReload = false;
+	
+	// ---- ---- ---- ---- ---- ---- ----
+	if (pSearch.test(URL) && pGoogle.test(URL)){
+		canReload = true;
+		urlTemp = safeIt(URL, 'safe', 'active');
+	}
+	else if (pSearch.test(URL) && pBing.test(URL)){
+		canReload = true;
+		urlTemp = safeIt(URL, 'adlt', 'strict');
+	}
+	else if (pSearch.test(URL) && pYahoo.test(URL)){
+		canReload = true;
+		urlTemp = safeIt(URL, 'vm', 'r');
+	}
+	else if (pSearch.test(URL) && pYandex.test(URL)){
+		canReload = true;
+		urlTemp = changeYandex(URL);
+	}
+	
+	// Loads a new and safe URL if necessary 
+	if (URL != urlTemp && canReload){
+		console.log("trocou :v");
+		return{
+			redirectUrl : urlTemp
+		};
+	}
+}
+
+/*
+	This function just substitite Yandex by Google, once there is any known parameter
+	enforcing SafeSearch in Yandex (If you know one, please open a request or issue).
+*/
+
+function changeYandex(url){
+	let params = new URL(url).searchParams;
+	var parameter = params.get("text");
+	
+	var newURL = "https://www.google.com/search?q=" + parameter + "&safe=active&gws_rd=cr";
+	console.log("New URL : " + newURL);
+	return	newURL;
+}
+
+/*
+	This function just canges or generates a safe URL if necessary.
+*/
+
+function safeIt(url, paramName, paramValue){
+	console.log("Seems to be working, isn't it? Look at the url : " + url);
+
+	let params = new URL(url).searchParams;
+	var parameter = params.get(paramName);
+
+	console.log("SS parameter : " + parameter);
+
+	if (parameter === null){
+		url += ('&' + paramName + "=" + paramValue);
+		console.log("New URL : " + url);
+	}
+	
+	else if (parameter != paramValue){
+		url = replaceUrlParam(url, paramName, paramValue);
+		console.log("New URL : " + url);
+	}
+	
+	return url;	
+}
+
+/*
+	Replace url SafeSearch parameter if necessary
+	Author: stenix (StackOverflow user)
+	Source: http://stackoverflow.com/a/20420424/3248289
+*/
+
+function replaceUrlParam(url, paramName, paramValue){
+    if(paramValue == null)
+        paramValue = '';
+    var pattern = new RegExp('\\b('+paramName+'=).*?(&|$)')
+    if(url.search(pattern)>=0){
+        return url.replace(pattern,'$1' + paramValue + '$2');
+    }
+    return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue 
+}
